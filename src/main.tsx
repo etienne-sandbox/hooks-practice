694dcffffe3ba38c033e160e0a9cef7a6924d92f
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-02-02 19:49:58 +0100

    Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { App } from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <p>Hello React</p>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
